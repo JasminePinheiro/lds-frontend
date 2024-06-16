87ca0e4ba22b758477f3cc7437966c8a529aba34
Jasmine Pinheiro <jasminepinheiro14@gmail.com>
2024-06-16 12:46:42 -0300

    Remoção do teste para adicionar eventos no calendário

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { EventModel } from '../../controllers/models/event-user';
-import { Firestore, addDoc, collection } from '@angular/fire/firestore';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -10,26 +9,5 @@ import { Firestore, addDoc, collection } from '@angular/fire/firestore';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
-// firestore = inject(Firestore)
-
-//   async testeAddEvent() {
-//     try {
-//       const newEvent: EventModel = {
-//         end_date: 123,
-//         event_name: "mine",
-//         start_date: 123,
-//         username: "mine"
-//       }
-//       alert("passou")
-
-//     await  addDoc(collection(this.firestore, 'eventos'), newEvent)
-//     console.log(this.firestore);
-    
-
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
   
 }
